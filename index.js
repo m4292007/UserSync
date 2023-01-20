81aca4c60fe6efda35c4ec4f5950ec792cd9762d
Krzysztof Desput <k.desput@clearcode.cc>
2023-01-20 15:03:41 +0100

    ACSD-4 Add HTTPS support and fix setting cookie

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express')
+const cors = require('cors')
+const https = require('https')
+const http = require('http')
+const fs = require('fs')
+require('dotenv').config()
+
 const app = express()
-const port = 80
+
+app.use(
+    cors({
+        origin: true,
+        credentials: true,
+    })
+);
 
 app.get('/UserSync/uid', (req, res) => {
     const u_id = req.query.u_id
-    res.cookie('b_id', u_id, { domain: 'admaru.net', maxAge: 31556926 })
+    res.cookie('b_id', u_id, { domain: 'admaru.net', maxAge: 31556926000, secure: true, sameSite: 'none' })
     res.send(u_id)
 })
 
@@ -17,6 +29,18 @@ app.get('/testdsp', (req, res) => {
     res.redirect('/UserSync/uid?p_id=1000&u_id=ZNDIFUA5V4UV69954E4OK5S2I')
 })
 
-app.listen(port, () => {
-    console.log(`User sync service listening on port ${port}`)
+http.createServer(app).listen(80, () => {
+    console.log(`User sync service listening on port 80`)
 })
+
+if (process.env.HTTPS_CERT_PATH) {
+    const httpsOptions = {
+        key: fs.readFileSync(`${process.env.HTTPS_CERT_PATH}/privkey.pem`),
+        cert: fs.readFileSync(`${process.env.HTTPS_CERT_PATH}/cert.pem`),
+        ca: fs.readFileSync(`${process.env.HTTPS_CERT_PATH}/chain.pem`)
+    }
+
+    https.createServer(httpsOptions, app).listen(443, () => {
+        console.log(`User sync service listening on port 443`)
+    })
+}
